8ebbda6a5390b66519a139e74779da78db5f87d1
luchsonice <s194369@student.dtu.dk>
2024-02-16 13:39:22 +0100

    Initial Commit

---
+++ test.sh
@@ -0,0 +1,6 @@
+python demo/demo.py \
+--config-file cubercnn://omni3d/cubercnn_DLA34_FPN.yaml \
+--input-folder "datasets/coco_examples" \
+--threshold 0.25 --display \
+MODEL.WEIGHTS cubercnn://omni3d/cubercnn_DLA34_FPN.pth \
+OUTPUT_DIR output/demo 
