ed0bc7bc2bd7c0e635e0bb61ee3318cf6f0c9361
Andreas Lau Hansen <andreasLauH@outlook.com>
2024-02-21 16:10:43 +0100

    mini for relevant image

--- datasetminify.py
+++ datasetminify.py
@@ -7,8 +7,9 @@ def minify_dataset(path, num_images=10):
     new_file = {}
     new_file['info'] = data['info']
     new_file['images'] = data['images'][:num_images]
-    new_file['categories'] = data['categories'][:num_images]
-    new_file['annotations'] = data['annotations'][:num_images]
+    new_file['categories'] = data['categories']
+    # grab only annotation for the image ids
+    new_file['annotations'] = [ann for ann in data['annotations'] if ann['image_id'] in [img['id'] for img in new_file['images']]]
     
     with open(path.replace('.json', '_mini.json'), 'w') as f:
         json.dump(new_file, f)
