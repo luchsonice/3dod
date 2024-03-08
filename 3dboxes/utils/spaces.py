823c48d149d52894e56904b0b9e800e16f1ad72f
luchsonice <s194369@student.dtu.dk>
2024-03-08 13:48:57 +0100

    more structures

--- 3dboxes/utils/spaces.py
+++ 3dboxes/utils/spaces.py
@@ -1,5 +1,6 @@
 import torch
 from cubercnn import util
+import numpy as np
 
 class Box:
     '''
@@ -46,67 +47,6 @@ class Box:
 
 
 
-
-class Bube:
-    '''
-    3D box on the 2D image plane in the format [c1, c2, w, h, l, R, K]
-
-    Args:
-        c1: The x coordinate of the center of the box.
-        c2: The y coordinate of the center of the box.
-        w: The width of the box in meters.
-        h: The height of the box in meters.
-        l: The length of the box in meters.
-        R: The 3D rotation matrix of the box.
-        K: The 3D camera matrix of the box.
-
-
-                      _____________________ 
-                    /|                    /|
-                   / |                   / |
-                  /  |                  /  |
-                 /___|_________________/   |
-                |    |                 |   | h
-                |    |                 |   |
-                |    |                 |   |
-                |    |   (c1,c2)       |   |
-                |    |_________________|___|
-                |   /                  |   /
-                |  /                   |  /
-                | /                    | / l
-                |/_____________________|/
-                            w             
-    '''
-    def __init__(self,tensor: torch.Tensor) -> None:
-        self.tensor = tensor
-        self.c1 = tensor[0]
-        self.c2 = tensor[1]
-        self.width = tensor[2]
-        self.height = tensor[3]
-        self.length = tensor[4]
-        self.roation = tensor[5]
-
-        if self.width < 0:
-            raise ValueError('Width must be greater than 0. Did you make sure that the input is in the correct order? (c1, c2, w, h, l, p)')
-        if self.height < 0:
-            raise ValueError('Height must be greater than 0. Did you make sure that the input is in the correct order? (c1, c2, w, h, l, p)')
-        if self.length < 0:
-            raise ValueError('Length must be greater than 0. Did you make sure that the input is in the correct order? (c1, c2, w, h, l, p)')
-        
-
-    def get_all_corners(self) -> torch.Tensor:
-        '''
-        It returns the 8 corners of the bube in the format [x, y]
-        '''
-        ul = self.roation@[self.c1-self.width/2, self.c2+self.height/2]
-
-        return True #torch.tensor([ul, ur, bl, br, dul, dur, dbl, dbr])
-    
-
-
-
-
-
 class Cube:
     '''
     3D box in the format [c1, c2, c3, w, h, l, R]
@@ -136,11 +76,11 @@ class Cube:
                 |/_____________________|/
                             w             
     '''
-    def __init__(self,tensor: torch.Tensor) -> None:
+    def __init__(self,tensor: torch.Tensor, R: torch.Tensor) -> None:
         self.tensor = tensor
         self.center = tensor[:3]
         self.dimensions = tensor[3:6]
-        self.rotation = tensor[6:]
+        self.rotation = R
 
         if self.dimensions[0] < 0:
             raise ValueError('Width must be greater than 0. Did you make sure that the input is in the correct order? (c1, c2, c3, w, h, l, p)')
@@ -149,14 +89,62 @@ class Cube:
         if self.dimensions[2] < 0:
             raise ValueError('Length must be greater than 0. Did you make sure that the input is in the correct order? (c1, c2, c3, w, h, l, p)')
         
-        if len(self.rotation) != 9:
-            raise ValueError('Rotation must be a 9D vector')
-        self.rotation = self.rotation.reshape(3,3)
+        if self.rotation.shape != (3,3):
+            raise ValueError('Rotation must be a 3x3 matrix.')
 
-        self.cube = util.mesh_cuboid(self.center+self.dimensions, self.rotation)
+        color = [c/255.0 for c in util.get_color()]
+        self.cube = util.mesh_cuboid(torch.cat((self.center,self.dimensions)), self.rotation, color=color)
+
+    def get_cube(self):
+        return self.cube
 
     def get_all_corners(self) -> torch.Tensor:
-        print(self.cube)
+        return self.cube.verts_list()
+
         
 
 
+class Bube:
+    '''
+    3D box on the 2D image plane in the format [cube, K]
+
+    Args:
+        cube: A cube.
+        K: The 3D camera matrix of the box.
+
+
+                      _____________________ 
+                    /|                    /|
+                   / |                   / |
+                  /  |                  /  |
+                 /___|_________________/   |
+                |    |                 |   | h
+                |    |                 |   |
+                |    |                 |   |
+                |    |   (c1,c2)       |   |
+                |    |_________________|___|
+                |   /                  |   /
+                |  /                   |  /
+                | /                    | / l
+                |/_____________________|/
+                            w             
+    '''
+    def __init__(self,cube: Cube, K: torch.Tensor) -> None:
+        self.cube = cube
+        self.K = K
+        self.center = cube.center[:2] # This is not correct, is it?
+        self.dimensions = cube.dimensions
+
+        if K.shape != (3,3):
+            raise ValueError('K must be a 3x3 matrix.')
+
+    def get_all_corners(self) -> torch.Tensor:
+        '''
+        It returns the 8 corners of the bube in the format [x, y]
+        '''
+        corners = self.cube.get_all_corners()
+        corners = torch.cat((corners, torch.ones(8,1)), dim=1)
+        corners = torch.mm(self.K, corners.t()).t()
+        corners = corners[:,:2]/corners[:,2].unsqueeze(1)
+
+        return corners
