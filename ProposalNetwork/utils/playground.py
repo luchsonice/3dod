24d74e94c5558e16eb99b4646eeff0263cdaffcf
luchsonice <s194369@student.dtu.dk>
2024-03-12 11:48:48 +0100

    cleaner and faster implementation of rotation matrix

--- ProposalNetwork/utils/playground.py
+++ ProposalNetwork/utils/playground.py
@@ -1,9 +1,5 @@
 from ProposalNetwork.proposals.proposals import setup_depth_model, depth_of_images
 
-
-# Import stuff above and continue with real depth input (havent checked code below). 
-
-
 from spaces import Box, Bube, Cube
 from conversions import bube_to_box, cube_to_bube, cube_to_box
 from utils import compute_rotation_matrix_from_ortho6d, make_random_box, propose
@@ -49,32 +45,27 @@ plt.savefig(os.path.join('/work3/s194369/3dod/ProposalNetwork/output/trash', 'te
 
 
 
-
+# Get image and scale intrinsics
 with open('ProposalNetwork/proposals/network_out.pkl', 'rb') as f:
         batched_inputs, images, features, proposals, Ks, gt_instances, im_scales_ratio, instances = pickle.load(f)
-
-#####################
-
 input_format = 'BGR'
 img = batched_inputs[0]['image']
 img = convert_image_to_rgb(img.permute(1, 2, 0), input_format)
 img_3DPR = np.ascontiguousarray(img.copy()[:, :, [2, 1, 1]]) # BGR
 input = batched_inputs[0]
+
 K = torch.tensor(input['K'])
 scale = input['height']/img.shape[0]
-
 K_scaled = torch.tensor(
     [[1/scale, 0 , 0], [0, 1/scale, 0], [0, 0, 1.0]], 
     dtype=torch.float32) @ K
 
-
-# 2 box
+# Get 2 proposal boxes
 box_size = min(len(proposals[0].proposal_boxes), 1)
 v_pred = Visualizer(img, None)
 v_pred = v_pred.overlay_instances(
     boxes=proposals[0].proposal_boxes[0:box_size].tensor.cpu().numpy()
 )
-
 box = torch.tensor(proposals[0].proposal_boxes[0:box_size].tensor.cpu().numpy()[0])
 box_width = box[2]-box[0]
 box_height = box[3]-box[1]
@@ -82,12 +73,13 @@ box_center_x = box[0]+box_width/2
 box_center_y = box[1]+box_height/2
 reference_box = Box(torch.tensor([box_center_x,box_center_y]),torch.tensor([box_width,box_height]))
 
+# Get depth info
 depth_model = 'zoedepth'
-# the local:: thing of the model path is just to indicate that the model is loaded local storage
 pretrained_resource = 'local::depth/checkpoints/depth_anything_metric_depth_indoor.pt'
 model = setup_depth_model(depth_model, pretrained_resource)
-
 depth_image = depth_of_images(img, model)
+
+# Get Proposals
 number_of_proposals = 4
 pred_cubes = propose(reference_box, depth_image, K_scaled, img.shape[:2],number_of_proposals=number_of_proposals)
 pred_meshes = []
@@ -95,15 +87,12 @@ for i in range(number_of_proposals):
     cube = pred_cubes[i].get_cube()
     pred_meshes.append(cube.__getitem__(0).detach())
 
-
+# Plot
+fig = plt.figure()
+ax = fig.add_subplot(111)
 prop_img = v_pred.get_image()
 img_3DPR = vis.draw_scene_view(prop_img, K_scaled.cpu().numpy(), pred_meshes, mode='front', blend_weight=0.0, blend_weight_overlay=0.85)
 vis_img_3d = img_3DPR.astype(np.uint8)
-
-
-# Plot bube on 2D plane
-fig = plt.figure()
-ax = fig.add_subplot(111)
 ax.imshow(vis_img_3d); ax.axis('off')
 #ax.plot(torch.cat((pred_box.get_all_corners()[:,0],pred_box.get_all_corners()[0,0].reshape(1))),torch.cat((pred_box.get_all_corners()[:,1],pred_box.get_all_corners()[0,1].reshape(1))),color='b')
 ax.plot(torch.cat((reference_box.get_all_corners()[:,0],reference_box.get_all_corners()[0,0].reshape(1))),torch.cat((reference_box.get_all_corners()[:,1],reference_box.get_all_corners()[0,1].reshape(1))),color='purple')
--- ProposalNetwork/utils/utils.py
+++ ProposalNetwork/utils/utils.py
@@ -2,32 +2,25 @@ import torch
 from spaces import Box, Bube, Cube
 from conversions import bube_to_box, cube_to_bube, cube_to_box
 
-# batch*n
 def normalize_vector(v):
-    batch=v.shape[0]
-    v_mag = torch.sqrt(v.pow(2).sum(1))# batch
+    v_mag = torch.sqrt(v.pow(2).sum())
     v_mag = torch.max(v_mag, torch.autograd.Variable(torch.FloatTensor([1e-8])))
-    v_mag = v_mag.view(batch,1).expand(batch,v.shape[1])
+    v_mag = v_mag.view(1,1).expand(1,v.shape[0])
     v = v/v_mag
 
-    return v
+    return v[0]
     
-# u, v batch*n
 def cross_product(u, v):
-    batch = u.shape[0]
-    #print (u.shape)
-    #print (v.shape)
-    i = u[:,1]*v[:,2] - u[:,2]*v[:,1]
-    j = u[:,2]*v[:,0] - u[:,0]*v[:,2]
-    k = u[:,0]*v[:,1] - u[:,1]*v[:,0]
+    i = u[1]*v[2] - u[2]*v[1]
+    j = u[2]*v[0] - u[0]*v[2]
+    k = u[0]*v[1] - u[1]*v[0]
+    out = torch.cat((i.view(1,1), j.view(1,1), k.view(1,1)),1)
         
-    out = torch.cat((i.view(batch,1), j.view(batch,1), k.view(batch,1)),1)#batch*3
-        
-    return out
+    return out[0]
 
 def compute_rotation_matrix_from_ortho6d(poses):
-    x_raw = poses[:,0:3]#batch*3
-    y_raw = poses[:,3:6]#batch*3
+    x_raw = poses[0:3]#batch*3
+    y_raw = poses[3:6]#batch*3
         
     x = normalize_vector(x_raw) #batch*3
     z = cross_product(x,y_raw) #batch*3
@@ -37,9 +30,9 @@ def compute_rotation_matrix_from_ortho6d(poses):
     x = x.view(-1,3,1)
     y = y.view(-1,3,1)
     z = z.view(-1,3,1)
-    matrix = torch.cat((x,y,z), 2) #batch*3*3
+    matrix = torch.cat((x,y,z), 2)[0]
 
-    return matrix[1] # TODO at some point all above should be converted to only output one rotation matrix
+    return matrix#[1] # TODO at some point all above should be converted to only output one rotation matrix
 
 def make_random_box(x_range, y_range, depth_image, w_range, h_range, l_range, im_shape):
     '''
@@ -58,7 +51,8 @@ def make_random_box(x_range, y_range, depth_image, w_range, h_range, l_range, im
     whl = torch.tensor([w, h, l])
 
     # R
-    rotation_matrix = compute_rotation_matrix_from_ortho6d(torch.vstack([torch.zeros(6),torch.rand(6)]))
+    #rotation_matrix = compute_rotation_matrix_from_ortho6d(torch.vstack([torch.zeros(6),torch.rand(6)]))
+    rotation_matrix = compute_rotation_matrix_from_ortho6d(torch.rand(6))
 
     return xyz, whl, rotation_matrix
 
@@ -97,5 +91,3 @@ def propose(reference_box, depth_image, K_scaled, im_shape, number_of_proposals=
     
     print('It took',c,'tries to find',number_of_proposals,'boxes.')
     return list_of_cubes
-
-
