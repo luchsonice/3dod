8b5d8292af30739357f914999f7903955ce47bdc
luchsonice <s194369@student.dtu.dk>
2024-03-12 15:50:01 +0100

    updates scoring

--- ProposalNetwork/scoring/scoringfunction.py
+++ ProposalNetwork/scoring/scoringfunction.py
@@ -3,6 +3,7 @@ from ProposalNetwork.utils.utils import intersection_over_proposal_area
 
 def scoring_proposal_in_2dgt(gt_box, proposal_box):
     IoA = intersection_over_proposal_area(gt_box,proposal_box)
+    #IoA = custom_softmax(IoA)
     return IoA
 
 def scoring_function(weights, gt_box, proposal_box):
