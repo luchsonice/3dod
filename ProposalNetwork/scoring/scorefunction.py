9f859a088abec2f4797044fd23d1a768f444d94c
luchsonice <s194369@student.dtu.dk>
2024-03-12 16:18:01 +0100

    renaming from scoring to score

--- ProposalNetwork/scoring/scorefunction.py
+++ ProposalNetwork/scoring/scorefunction.py
@@ -1,13 +1,13 @@
 import torch
 from ProposalNetwork.utils.utils import intersection_over_proposal_area, custom_mapping
 
-def scoring_proposal_in_2dgt(gt_box, proposal_box):
+def score_proposal_in_2dgt(gt_box, proposal_box):
     IoA = intersection_over_proposal_area(gt_box,proposal_box)
     IoA = custom_mapping(IoA)
     return IoA
 
-def scoring_function(weights, gt_box, proposal_box):
+def score_function(weights, gt_box, proposal_box):
     score = 1.0
-    score *= scoring_proposal_in_2dgt(gt_box, proposal_box)
+    score *= score_proposal_in_2dgt(gt_box, proposal_box)
 
     return score
