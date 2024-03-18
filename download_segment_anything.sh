94c9de3b89796dabcb1a3d1223d202866aa4c120
Andreas Lau Hansen <andreasLauH@outlook.com>
2024-03-18 11:32:46 +0100

    segmentation implemented

---
+++ download_segment_anything.sh
@@ -0,0 +1,4 @@
+# execute this from within the segment-anything directory
+cd segment-anything
+wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -O sam_vit_b_01ec64.pth
+cd ..
