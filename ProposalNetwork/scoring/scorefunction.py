4b2c3d073f6ce608c78b707dd4f41cd29179bd27
luchsonice <s194369@student.dtu.dk>
2024-03-18 11:44:26 +0100

    score segmentation bube not tested

--- ProposalNetwork/scoring/scorefunction.py
+++ ProposalNetwork/scoring/scorefunction.py
@@ -1,17 +1,27 @@
 import torch
-from ProposalNetwork.utils.utils import iou_2d, iou_3d, custom_mapping
+import numpy as np
+import cv2
+
+from ProposalNetwork.utils.utils import iou_2d, iou_3d, custom_mapping, mask_ioa
 
 def score_iou(gt_box, proposal_box):
     IoU = iou_2d(gt_box,proposal_box)
     #IoU = custom_mapping(IoU)
     return IoU
 
-def score_segmentation_ioa(bube, segmentation):
+def score_segmentation(bube_corners, segmentation_mask):
     '''
     IoA between segmentation and bube.
     '''
+    bube_mask = np.zeros(segmentation_mask.size, dtype=np.uint8)
     
-    return 1
+    # Remove "inner" points (2) and put others in correct order 
+    # Calculate the convex hull of the points which also orders points correctly
+    polygon_points = cv2.convexHull(np.array(bube_corners), returnPoints=True)
+
+    cv2.fillPoly(bube_mask, [polygon_points], 1)
+
+    return mask_ioa(segmentation_mask, bube_mask)
 
 def score_function(weights, gt_box, proposal_box):
     score = 1.0
