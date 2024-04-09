b2e720da22d11e254e57b4c7f4ff737eccc4f528
luchsonice <s194369@student.dtu.dk>
2024-04-09 09:49:36 +0200

    added possibility for viable dimensions in norm space

--- ProposalNetwork/utils/conversions.py
+++ ProposalNetwork/utils/conversions.py
@@ -29,29 +29,27 @@ def Boxes_to_list_of_Box(Boxes):
     detectron_boxes = Boxes.tensor
     return [Box(detectron_boxes[i,:]) for i in range(detectron_boxes.shape[1])]
 
-def pixel_to_normalised_space(pixel_coord, im_shape):
+def pixel_to_normalised_space(pixel_coord, im_shape, norm_shape):
     '''
-    pixel_coord: Nx2
+    pixel_coord: List of length N
+    im_shape: List of length N
+    norm_shape: List of length N
     '''
-    pixel_coord = np.array(pixel_coord)
-    if np.shape(pixel_coord) == (2,):
-        pixel_coord = pixel_coord.reshape(1,2) # TODO should be for general N
+    new_coords = np.array(pixel_coord).astype(np.float32)
 
-    new_height, new_width = 2,2
-    old_width = im_shape[0]
-    old_height = im_shape[1]
-    new_coords = pixel_coord.astype(np.float32)
-    new_coords[:, 0] -= 0.5 * old_width
-    new_coords[:, 1] -= 0.5 * old_height
-    new_coords[:, 0] *= new_width / old_width
-    new_coords[:, 1] *= new_height / old_height
+    for i in range(len(pixel_coord)):
+        old_dim = im_shape[i]
+        new_dim = norm_shape[i]
+
+        new_coords[i] -= 0.5 * old_dim
+        new_coords[i] *= new_dim / old_dim
     
-    return new_coords
+    return new_coords # TODO feel like its missing a line, something if normshape is not 2. Where did we take inspiration from? A library?
 
-def normalised_space_to_pixel(coords,im_shape):
+def normalised_space_to_pixel(coords,im_shape): # TODO needs to be updated
     coords = np.array(coords)
     if np.shape(coords) == (2,):
-        coords = coords.reshape(1,2) # TODO should be for general N
+        coords = coords.reshape(1,2)
 
     new_height, new_width = im_shape[1],im_shape[0]
     old_width = 2
