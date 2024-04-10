correct the random rotation generation

ProposalNetwork/proposals/proposals.py
@@ -1,9 +1,8 @@
-from ProposalNetwork.utils.spaces import Box, Cube
-from ProposalNetwork.utils.conversions import pixel_to_normalised_space, normalised_space_to_pixel, pixel_to_normalised_space_torch
+from ProposalNetwork.utils.spaces import Cube
+from ProposalNetwork.utils.conversions import pixel_to_normalised_space, pixel_to_normalised_space_torch
 from ProposalNetwork.utils.utils import make_cube, is_gt_included, make_cubes_parallel
 import torch
 import numpy as np
-from cubercnn import util
 
 def propose_random(reference_box, depth_image, K_scaled, im_shape, number_of_proposals=1):
     '''
@@ -103,7 +102,6 @@ def propose2(reference_box, depth_image, priors, im_shape, number_of_proposals=1
     w_prior = torch.tensor([priors[0][0], priors[1][0]], device=depth_image.device)
     h_prior = torch.tensor([priors[0][1], priors[1][1]], device=depth_image.device)
     l_prior = torch.tensor([priors[0][2], priors[1][2]], device=depth_image.device)
-
 
     list_of_cubes = []
     # Predict cubes

ProposalNetwork/utils/spaces.py
@@ -46,8 +46,12 @@ class Box:
 
         self.width = self.x2 - self.x1
         self.height = self.y2 - self.y1
-        self.center = torch.tensor([self.x1 + self.width/2, self.y1+self.height/2])
         self.area = self.width * self.height
+
+    @property
+    def center(self) -> torch.Tensor:
+        '''this function is accessed like Box.center'''
+        return torch.tensor([self.x1 + self.width/2, self.y1+self.height/2], device=self.x1.device)
 
     @property
     def box(self) -> Boxes:
@@ -75,7 +79,6 @@ class Box:
         Args:
             device: The device to move the tensors to (e.g., 'cuda', 'cpu').
         '''
-        self.center = self.center.to(device)
         self.width = self.width.to(device)
         self.height = self.height.to(device)
         self.area = self.area.to(device)
@@ -119,21 +122,20 @@ class Cube:
         self.dimensions = tensor[3:6]
         self.rotation = R
 
-        if self.dimensions[0] < 0:
-            raise ValueError('Width must be greater than 0.')
-        if self.dimensions[1] < 0:
-            raise ValueError('Height must be greater than 0.')
-        if self.dimensions[2] < 0:
-            raise ValueError('Length must be greater than 0.')
+        # if self.dimensions[0] < 0:
+        #     raise ValueError('Width must be greater than 0.')
+        # if self.dimensions[1] < 0:
+        #     raise ValueError('Height must be greater than 0.')
+        # if self.dimensions[2] < 0:
+        #     raise ValueError('Length must be greater than 0.')
         
-        if self.rotation.shape != (3,3):
-            raise ValueError('Rotation must be a 3x3 matrix.')
+        # if self.rotation.shape != (3,3):
+        #     raise ValueError('Rotation must be a 3x3 matrix.')
 
-        color = [c/255.0 for c in util.get_color()]
-        self.cube = util.mesh_cuboid(torch.cat((self.center,self.dimensions)), self.rotation, color=color)
 
     def get_cube(self):
-        return self.cube
+        color = [c/255.0 for c in util.get_color()]
+        return util.mesh_cuboid(torch.cat((self.center,self.dimensions)), self.rotation, color=color)
 
     def get_all_corners(self) -> torch.Tensor:
         return self.cube.verts_list()[0]

ProposalNetwork/utils/utils.py
@@ -98,13 +98,18 @@ def make_cubes_parallel(x_range, y_range, z, w_prior, h_prior, l_prior, number_o
     whl = torch.stack([w, h, l], 1)
 
     # R
-    #rotation_matrix = compute_rotation_matrix_from_ortho6d(torch.rand(6)) # Use this when learnable
-    rx = torch.rand(number_of_proposals) * np.pi - np.pi/2
-    ry = torch.rand(number_of_proposals) * np.pi - np.pi/2
-    rz = torch.rand(number_of_proposals) * np.pi - np.pi/2
-    rotation_matrix = util.euler2mat_torch(torch.stack([rx,ry,rz],1))
+    rotation_matrix = randn_orthobasis_torch(number_of_proposals) 
     
     return xyz, whl, rotation_matrix
+
+def randn_orthobasis_torch(num_samples=1):
+    z = torch.randn(num_samples, 3, 3)
+    z = z / torch.norm(z, p=2, dim=-1, keepdim=True)
+    z[:, 0] = torch.cross(z[:, 1], z[:, 2], dim=-1)
+    z[:, 0] = z[:, 0] / torch.norm(z[:, 0], dim=-1, keepdim=True)
+    z[:, 1] = torch.cross(z[:, 2], z[:, 0], dim=-1)
+    z[:, 1] = z[:, 1] / torch.norm(z[:, 1], dim=-1, keepdim=True)
+    return z
 
 def is_box_included_in_other_box(reference_box, proposed_box):
     reference_corners = reference_box.get_all_corners()