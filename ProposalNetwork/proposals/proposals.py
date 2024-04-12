83d0600ba3761f627283014cee9dce0d361a30ef
luchsonice <s194369@student.dtu.dk>
2024-04-12 15:37:24 +0200

    further improvements

--- ProposalNetwork/proposals/proposals.py
+++ ProposalNetwork/proposals/proposals.py
@@ -18,7 +18,7 @@ def propose(reference_box, depth_image, priors, im_shape, K, number_of_proposals
     '''
     ####### Center
     # Removing the outer % on each side of range for center point
-    m = 4
+    m = 5
     x_range_px = torch.tensor([reference_box.x1+reference_box.width/(m+1),reference_box.x2-reference_box.width/m],device=depth_image.device)
     y_range_px = torch.tensor([reference_box.y1+reference_box.height/m,reference_box.y2-reference_box.height/m],device=depth_image.device)
     # Find depths
@@ -37,19 +37,21 @@ def propose(reference_box, depth_image, priors, im_shape, K, number_of_proposals
     y = d * torch.sin(angle_y)
     # Calculatre z
     dx = torch.sqrt(d**2 - y**2)
-    z = torch.sqrt(dx**2 - x**2)
+    z_tmp = torch.sqrt(dx**2 - x**2)
     
 
     # Should also have min and max
     w_prior = torch.tensor([priors[0][0], priors[1][0]], device=depth_image.device)
     h_prior = torch.tensor([priors[0][1], priors[1][1]], device=depth_image.device)
     l_prior = torch.tensor([priors[0][2], priors[1][2]], device=depth_image.device)
-    w = sample_normal_greater_than_para(w_prior[0], w_prior[1]/2, torch.tensor(0.1), w_prior[0] + 1 * w_prior[1], number_of_proposals) # NOTE Halving std right now. Improves but sketchy
-    h = sample_normal_greater_than_para(h_prior[0], h_prior[1]/2, torch.tensor(0.1), h_prior[0] + 1 * h_prior[1], number_of_proposals)
+    w = sample_normal_greater_than_para(w_prior[0], w_prior[1]/2, torch.tensor(0.1), w_prior[0] + 0.5 * w_prior[1], number_of_proposals) # NOTE Halving std right now. Improves but sketchy
+    h = sample_normal_greater_than_para(h_prior[0], h_prior[1]/2, torch.tensor(0.1), h_prior[0] + 0.5 * h_prior[1], number_of_proposals)
     l = sample_normal_greater_than_para(l_prior[0], l_prior[1]/2, torch.tensor(0.05),l_prior[0] + 0.4 * l_prior[1], number_of_proposals)
     whl = torch.stack([w, h, l], 1)
 
-    z = z+l/2
+    # Finish z
+    z = z_tmp+l/2
+    z = sample_normal_greater_than_para(torch.median(z), torch.std(z), torch.tensor(0),torch.tensor(7), number_of_proposals)
     xyz = torch.stack([x, y, z], 1)
 
     # Pose
@@ -65,14 +67,14 @@ def propose(reference_box, depth_image, priors, im_shape, K, number_of_proposals
     for i in range(number_of_proposals):
         pred_cube = Cube(torch.cat((xyz[i], whl[i]), dim=0),rotation_matrix)
         list_of_cubes.append(pred_cube)
-
+    
     # Statistics
     stds = 2
     stat_x = gt_in_norm_range([torch.min(x),torch.max(x)],gt_cube.center[0])
     stat_y = gt_in_norm_range([torch.min(y),torch.max(y)],gt_cube.center[1])
     stat_z = gt_in_norm_range([torch.min(z),torch.max(z)],gt_cube.center[2])
-    stat_w = gt_in_norm_range([torch.max(w_prior[0]-stds*w_prior[1],torch.tensor(0.1)),torch.min(w_prior[0]+stds*w_prior[1],w_prior[0]+1*w_prior[1])],gt_cube.dimensions[0])
-    stat_h = gt_in_norm_range([torch.max(h_prior[0]-stds*h_prior[1],torch.tensor(0.1)),torch.min(h_prior[0]+stds*h_prior[1],h_prior[0]+1*h_prior[1])],gt_cube.dimensions[1])
+    stat_w = gt_in_norm_range([torch.max(w_prior[0]-stds*w_prior[1],torch.tensor(0.1)),torch.min(w_prior[0]+stds*w_prior[1],w_prior[0]+0.5*w_prior[1])],gt_cube.dimensions[0])
+    stat_h = gt_in_norm_range([torch.max(h_prior[0]-stds*h_prior[1],torch.tensor(0.1)),torch.min(h_prior[0]+stds*h_prior[1],h_prior[0]+0.5*h_prior[1])],gt_cube.dimensions[1])
     stat_l = gt_in_norm_range([torch.max(l_prior[0]-stds*l_prior[1],torch.tensor(0.05)),torch.min(l_prior[0]+stds*l_prior[1],l_prior[0]+0.4*l_prior[1])],gt_cube.dimensions[2])
     angles = util.mat2euler(gt_cube.rotation)
     stat_rx = gt_in_norm_range(torch.tensor([-np.pi,np.pi]),torch.tensor(angles[0]))
