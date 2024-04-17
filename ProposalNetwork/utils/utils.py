4565db0ef3825255a4a421102aef94f911013256
luchsonice <s194369@student.dtu.dk>
2024-04-17 14:41:13 +0200

    correct utils

--- ProposalNetwork/utils/utils.py
+++ ProposalNetwork/utils/utils.py
@@ -74,16 +74,11 @@ def make_cube(x_range, y_range, z, w_prior, h_prior, l_prior, ground_normal=None
     # R
     #rotation_matrix = compute_rotation_matrix_from_ortho6d(torch.rand(6)) # Use this when learnable
     if ground_normal is None:
-<<<<<<< HEAD
-        rotation_matrix = randn_orthobasis_torch(1)
-  
-=======
         rotation_matrix = randn_orthobasis_torch(1).squeeze(0)
     else:
         angles = np.linspace(0, np.pi, 36) # 5 degree steps
         rotation_matrix = torch.from_numpy(orthobasis_from_normal(ground_normal, 0))
     
->>>>>>> e33ce76fe65ee0f43969d1ab827c8a042d023d59
     return xyz, whl, rotation_matrix
 
 def make_cubes_parallel(x_range, y_range, z, w_prior, h_prior, l_prior, number_of_proposals=1):
