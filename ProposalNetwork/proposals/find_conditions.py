fix adjustment of center to be without leakage

ProposalNetwork/proposals/find_conditions.py
@@ -4,11 +4,11 @@ import matplotlib.pyplot as plt
 
 # To find txt files , needs to be run in proposal before final center point is found
 """
-with open("conditions.txt", "a") as f:
+with open("ProposalNetwork/proposals/conditions.txt", "a") as f:
     # Redirect the standard output to the file
     sys.stdout = f
     # Your print statements here
-    print(f"{int(y_range_px[1].item())},{((gt_cube.center[1].item()-torch.min(y).item())/y_width.item()):.2f}")
+    print(f"{torch.median(z)},{gt_cube.center[2].item()}")
 # Reset the standard output
 sys.stdout = sys.__stdout__
 """
@@ -18,7 +18,7 @@ x_cond = []
 y_cond = []
 
 # Read data from the file
-with open("conditions_z.txt", "r") as file:
+with open("ProposalNetwork/proposals/conditions_x.txt", "r") as file:
     for line in file:
         # Split each line by comma
         parts = line.strip().split(",")
@@ -43,7 +43,7 @@ sys.stdout = sys.__stdout__
 
 plt.figure()
 plt.scatter(x_cond,y_cond)
-plt.savefig("z_values_to_find.png", dpi=300, bbox_inches='tight')
+plt.savefig("ProposalNetwork/proposals/x_values_to_find.png", dpi=300, bbox_inches='tight')
 plt.close()
 
 

ProposalNetwork/proposals/proposals.py
@@ -55,16 +55,16 @@ def propose(reference_box, depth_image, priors, im_shape, K, number_of_proposals
         return coef[0] * x + coef[1]
     
     # x
-    x_coefficients = np.array([1.18312167, 0.0056523])
+    x_coefficients = np.array([1.15, 0])
     x = sample_normal_greater_than_para(fun(torch.median(x),x_coefficients), torch.std(x)*0.7, torch.tensor(-8),torch.tensor(8), number_of_proposals) # TODO Run without limits
     
     # y
-    y_coefficients  = np.array([1.14174714, -0.01602377])
+    y_coefficients  = np.array([1.1, 0])
     y = sample_normal_greater_than_para(fun(torch.median(y),y_coefficients), torch.std(y)*0.7, torch.tensor(-3),torch.tensor(3), number_of_proposals)
     
     # z
-    z = z_tmp+l
-    z_coefficients = np.array([0.82269712, 0.30410975])
+    z = z_tmp+l/2
+    z_coefficients = np.array([0.85, 0.35])
     z = sample_normal_greater_than_para(fun(torch.median(z),z_coefficients), torch.std(z) * 1.2, torch.tensor(-0.5),torch.tensor(100), number_of_proposals)
 
     xyz = torch.stack([x, y, z], 1)