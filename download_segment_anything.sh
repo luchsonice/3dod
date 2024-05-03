377c7e89397264846db3289778e70bfe4d43d937
Andreas Lau <andreaslauh@outlook.com>
2024-05-03 09:24:35 +0200

    download script for sam-hq

--- download_segment_anything.sh
+++ download_segment_anything.sh
@@ -2,3 +2,7 @@
 cd segment-anything
 wget https://dl.fbaipublicfiles.com/segment_anything/sam_vit_b_01ec64.pth -O sam_vit_b_01ec64.pth
 cd ..
+
+cd sam-hq
+wget https://huggingface.co/lkeab/hq-sam/resolve/main/sam_hq_vit_b.pth -O sam_hq_vit_b.pth
+cd ..
