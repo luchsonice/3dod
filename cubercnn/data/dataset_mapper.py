4bd53ab375d167ee1a5cb4163d35e6bf7c5f148f
luchsonice <s194369@student.dtu.dk>
2024-05-09 15:50:25 +0200

    forgot s

--- cubercnn/data/dataset_mapper.py
+++ cubercnn/data/dataset_mapper.py
@@ -131,7 +131,7 @@ class DatasetMapper3D(DatasetMapper):
             else:
                 dataset_dict["ground_map"] = None
         elif self.mode == 'load_proposals':
-            with open(f"datasets/proposals/proposal_learn/{dataset_dict['image_id']}.pkl", 'rb') as f:
+            with open(f"datasets/proposals/proposals_learn/{dataset_dict['image_id']}.pkl", 'rb') as f:
                 proposals = pickle.load(f)
             dataset_dict["proposals"] = proposals
                                             
