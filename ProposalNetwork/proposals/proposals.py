fix off by one error

ProposalNetwork/proposals/proposals.py
@@ -49,8 +49,8 @@ def propose_z(reference_box, depth_image, priors, im_shape, K, number_of_proposa
     yt = rescale_interval(y, -1, 1)
     img_h, img_w = depth_image.shape
     z = torch.zeros_like(x)
-    x_scaled = rescale_interval(x, 0, img_w).to(dtype=int)
-    y_scaled = rescale_interval(y, 0, img_h).to(dtype=int)
+    x_scaled = rescale_interval(x, 0, img_w-1).to(dtype=int)
+    y_scaled = rescale_interval(y, 0, img_h-1).to(dtype=int)
     for i in range(number_of_instances):
         z[i] = depth_image[y_scaled[i], x_scaled[i]]
     
@@ -94,8 +94,8 @@ def propose_xy_patch(reference_box, depth_image, priors, im_shape, K, number_of_
     yt = rescale_interval(y, (h_y).view(-1, 1), l_y.view(-1, 1))
     z = torch.zeros_like(x)
     # rescale the number so the fit the image size
-    x_scaled = rescale_interval(x, x_min.view(-1, 1), x_max.view(-1, 1)).to(dtype=int)
-    y_scaled = rescale_interval(y, y_min.view(-1, 1), y_max.view(-1, 1)).to(dtype=int)
+    x_scaled = rescale_interval(x, x_min.view(-1, 1), x_max.view(-1, 1)-1).to(dtype=int)
+    y_scaled = rescale_interval(y, y_min.view(-1, 1), y_max.view(-1, 1)-1).to(dtype=int)
     for i in range(number_of_instances):
         z[i] = depth_image[y_scaled[i], x_scaled[i]]
 
@@ -206,7 +206,7 @@ def propose_aspect_ratio(reference_box, depth_image, priors, im_shape, K, number
     # Dimensions
     w = rescale_interval(torch.rand(number_of_instances,number_of_proposals, device=reference_box.device), MIN_PROP_S, 2)
     #
-    ratios = [0.5, 0.66, 1, 1.33, 1.5, 1.67, 2, 3]
+    ratios = [0.33, 0.66, 1, 1.33, 1.67, 2, 3]
     h = torch.zeros_like(w)
     l = torch.zeros_like(w)
     for i in range(number_of_instances):

VisualiseGT.py
@@ -43,7 +43,8 @@ def load_gt(dataset='SUNRGBD', mode='test', single_im=True, filter=False):
     imgs = dataset.loadImgs(imgIds)
     if single_im:
         # img = random.choice(imgs)
-        img = imgs[730]
+        # 730 and 150 are used in the report
+        img = imgs[150]
         annIds = dataset.getAnnIds(imgIds=img['id'])
     else:
         # get all annotations
@@ -96,11 +97,13 @@ def plot_scene(image_path, output_dir, center_cams, dimensions_all, Rs, K, cats,
     
     image_name = util.file_parts(image_path)[1]
     print('File: {} with {} dets'.format(image_name, len(meshes)))
+    np.random.seed(0)
+    colors = [np.concatenate([np.random.random(3), np.array([0.6])], axis=0) for _ in range(len(meshes))]
 
     # Plot
     image = util.imread('datasets/'+image_path)
     if len(meshes) > 0:
-        im_drawn_rgb, im_topdown, _ = vis.draw_scene_view(image, np.array(K), meshes, text=meshes_text, scale=image.shape[0], blend_weight=0.5, blend_weight_overlay=0.85)
+        im_drawn_rgb, im_topdown, _ = vis.draw_scene_view(image, np.array(K), meshes, colors=colors, text=meshes_text, scale=image.shape[0], blend_weight=0.5, blend_weight_overlay=0.85)
 
         if False:
             im_concat = np.concatenate((im_drawn_rgb, im_topdown), axis=1)
@@ -109,7 +112,7 @@ def plot_scene(image_path, output_dir, center_cams, dimensions_all, Rs, K, cats,
         util.imwrite(im_drawn_rgb, os.path.join(output_dir, image_name+'_boxes.jpg'))
         util.imwrite(im_topdown, os.path.join(output_dir, image_name+'_novel.jpg'))
         v_pred = Visualizer(image, None)
-        v_pred = v_pred.overlay_instances(boxes=np.array(bboxes),)
+        v_pred = v_pred.overlay_instances(boxes=np.array(bboxes), assigned_colors=colors)
         util.imwrite(v_pred.get_image(), os.path.join(output_dir, image_name+'_pred_boxes.jpg'))
     else:
         print('No meshes')
@@ -483,11 +486,11 @@ def vol_over_cat(dataset):
 
     return True
 if __name__ == '__main__':
-    # show_data('SUNRGBD', filter_invalid=False, output_dir='output/playground/no_filter')  #{SUNRGBD,ARKitScenes,KITTI,nuScenes,Objectron,Hypersim}
-    # show_data('SUNRGBD', filter_invalid=True, output_dir='output/playground/with_filter')  #{SUNRGBD,ARKitScenes,KITTI,nuScenes,Objectron,Hypersim}
+    show_data('SUNRGBD', filter_invalid=False, output_dir='output/playground/no_filter')  #{SUNRGBD,ARKitScenes,KITTI,nuScenes,Objectron,Hypersim}
+    show_data('SUNRGBD', filter_invalid=True, output_dir='output/playground/with_filter')  #{SUNRGBD,ARKitScenes,KITTI,nuScenes,Objectron,Hypersim}
     # _ = category_distribution('SUNRGBD')
     # AP_vs_no_of_classes('SUNRGBD')
-    spatial_statistics('SUNRGBD')
+    # spatial_statistics('SUNRGBD')
     # AP3D_vs_AP2D('SUNRGBD')
     # init_dataloader()
     # vol_over_cat('SUNRGBD')