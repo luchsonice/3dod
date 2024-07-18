fix the AP2D calculation

cubercnn/modeling/meta_arch/rcnn3d.py
@@ -738,7 +738,12 @@ class BoxNet(nn.Module):
         # use the mask and the 2D box to predict the 3D box
         # proposals are ground truth for MABO plots and predictions for AP plots
         results = self.roi_heads(images, images_raw, combined_features, depth_maps, ground_maps, features, proposals, Ks, im_scales_ratio, experiment_type, proposal_function)
-        return results #[{'instances':results}]
+
+        if do_postprocess:
+            assert not torch.jit.is_scripting(), "Scripting is not supported for postprocess."
+            return GeneralizedRCNN._postprocess(results, batched_inputs, images.image_sizes)
+        else:
+            return results #[{'instances':results}]
     
     def visualize_training(self, batched_inputs, proposals, instances):
         """